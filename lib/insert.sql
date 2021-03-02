93c441c9acfc9d34737709f6f925cdd0905a8816
Amanda <ajnikrant@gmail.com>
2021-03-02 18:25:47 -0500

    commit tree 30cfea703e015f95060bf8f0ee5aee1b19626a29

---
+++ lib/insert.sql
@@ -0,0 +1,17 @@
+-- Mr. Chocolate
+-- Rowdy
+-- Tabitha
+-- Sergeant Brown
+-- Melissa
+-- Grinch
+-- Wendy
+-- unnamed
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocloate", 3, "M", "brown", "friendly", TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 8, "M", "light tan", "well, rowdy", FALSE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 2, "F", "brown", "sweet", TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Seargeant Brown", 12, "M", "black", "tough", FALSE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 3, "F", "brown", "friendly", TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 5, "M", "black", "tough", TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 2, "F", "light tan", "sweet", TRUE);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 3, "M", "brown", "deadly", FALSE);
